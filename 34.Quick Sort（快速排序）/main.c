800b77c17c3a864f203f7e4ff65857dcf5337cd8
Pei <1367890978@qq.com>
2018-06-20 18:38:36 +0800

    Initial Commit

---
+++ 34.Quick Sort（快速排序）/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  34.Quick Sort（快速排序）
+//
+//  Created by sunny&pei on 2018/6/20.
+//  Copyright © 2018年 sunny&pei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
